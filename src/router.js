5bbaf9e8971c502c5180bee47d4ef174f9d165dc
Reagan Cooper <reagan.cooper@bluesombrero.com>
2019-01-05 21:37:13 -0500

    add route guard

--- src/router.js
+++ src/router.js
@@ -2,20 +2,43 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 
+import firebase from "firebase/app";
+
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: "/",
       name: "home",
-      component: Home
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: "/login",
       name: "login",
       component: () =>
         import(/* webpackChunkName: "login" */ "./views/Login.vue")
+    },
+    {
+      path: "/create",
+      name: "create",
+      component: () =>
+        import(/* webpackChunkName: "create" */ "./views/Create.vue"),
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/review",
+      name: "review",
+      component: () =>
+        import(/* webpackChunkName: "review" */ "./views/Review.vue"),
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: "/about",
@@ -25,18 +48,18 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
-    },
-    {
-      path: "/create",
-      name: "create",
-      component: () =>
-        import(/* webpackChunkName: "create" */ "./views/Create.vue")
-    },
-    {
-      path: "/review",
-      name: "review",
-      component: () =>
-        import(/* webpackChunkName: "review" */ "./views/Review.vue")
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  if (requiresAuth && !currentUser) {
+    next("/login");
+  } else {
+    next();
+  }
+});
+
+export default router;
